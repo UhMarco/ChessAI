01faa707e829209a128c55d45aeb67a7baafc040
Marco <ddefygravity@gmail.com>
2021-08-19 18:14:19 +0200

    Play against a random opponent

---
+++ RandomBot.js
@@ -0,0 +1,22 @@
+class RandomBot {
+    constructor(colour = 'black') {
+        this.colour = colour == 'white' ? true : false;
+    }
+
+    move() {
+        const moves = [];
+        const pieces = this.colour ? board.whitePieces : board.blackPieces;
+        pieces.forEach(piece => {
+            piece.generateMoves();
+            piece.generateLegalMoves();
+            if (piece.moves.length) {
+                moves.push(piece);
+            }
+        });
+
+        const piece = moves[Math.floor(Math.random() * moves.length)];
+        const [x, y] = piece.moves[Math.floor(Math.random() * piece.moves.length)];
+        console.log(piece.type, board.getNotation(x, y).join(''));
+        board.move(x, y, piece);
+    }
+}
